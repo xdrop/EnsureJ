0f2805b68a2844e842a723dd7c4c89b9abb46255
xdrop <pastosdev@gmail.com>
2017-01-07 18:15:26 +0200

    Add Object checks

--- src/main/java/me/xdrop/ensurej/checks/CheckObject.java
+++ src/main/java/me/xdrop/ensurej/checks/CheckObject.java
@@ -1,4 +1,34 @@
 package me.xdrop.ensurej.checks;
 
+import me.xdrop.ensurej.Predicate;
+
 public class CheckObject {
+
+    public static Predicate<Object> isNull(){
+        return new Predicate<Object>() {
+            @Override
+            public boolean eval(Object in) {
+                return in == null;
+            }
+        };
+    }
+
+    public static Predicate<Object> isNotNull(){
+        return new Predicate<Object>() {
+            @Override
+            public boolean eval(Object in) {
+                return in != null;
+            }
+        };
+    }
+
+    public static Predicate<Object> equal(final Object other){
+        return new Predicate<Object>() {
+            @Override
+            public boolean eval(Object in) {
+                return in.equals(other);
+            }
+        };
+    }
+
 }
